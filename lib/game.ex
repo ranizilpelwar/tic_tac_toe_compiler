222c5d11eed79f44d989a4d2a4bdc7f9f1d6cd84
Rani Zilpelwar <rani@8thlight.com>
2018-08-23 09:12:27 -0400

    Fix board formatting and retry request when web server is down

--- lib/game.ex
+++ lib/game.ex
@@ -9,6 +9,10 @@ defmodule Game do
     end
 
     def execute_command(map) do
+        if (Map.has_key?(map, :timeout)) do
+            IO.puts "Waking up web server"
+            execute_command(map)
+        end
         if (Map.has_key?(map, :statuses) && map.statuses.game_over === true) do
             show_end_game_results()
         else
@@ -17,7 +21,7 @@ defmodule Game do
             method = MethodParser.parse(command)
             request = MethodParser.call(method)
             game_state = WebServerCommunicator.send_request(request)
-            IO.puts "Tic Tac Toe Board:"
+            IO.puts "\nTic Tac Toe Board:\n"
             board = game_state["game"]["board"]
             IO.puts format_board(board)
             execute_command(game_state)
@@ -30,16 +34,24 @@ defmodule Game do
     end
 
     def format_board(list) when length(list) == 0 do
-        "|"
+        "\n"
     end
 
     def format_board([head | tail]) do
-        "|" <> head <> conditionally_display_horizontal_bars(tail) <> format_board(tail)
+        conditionally_display_vertical_bars([head | tail]) <> head <> conditionally_display_horizontal_bars(tail) <> format_board(tail)
+    end
+
+    def conditionally_display_vertical_bars(list) do
+        if (Enum.member?([1,2,4,5,7,8], length(list))) do
+            "|"
+        else
+            ""
+        end
     end
 
     def conditionally_display_horizontal_bars(list) do
         if (length(list) == 3 || length(list) == 6) do
-            "|\n+=+=+=+\n"
+            "\n-+-+-\n"
         else
             ""
         end
